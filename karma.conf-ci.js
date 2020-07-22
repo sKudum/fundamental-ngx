6f6479073a84a6f1550d3b3b23a416f7d5a8e4f8
skudum <sushma.kudum@sap.com>
2020-07-22 21:07:21 +0530

    paths added

--- karma.conf-ci.js
+++ karma.conf-ci.js
@@ -1,67 +1,111 @@
 //Karma travsci configuration for saucelab
 
 'use strict';
+const { constants } = require('karma');
+const { join } = require('path');
 
-process.env.SAUCE_USERNAME = 'uiappframework';
-process.env.SAUCE_ACCESS_KEY = 'bbb2b274-6854-456a-aa18-6a311ad10b69';
+module.exports = function () {
 
+    // Use ENV vars on Travis and sauce.json locally to get credentials
 
-module.exports = function (config) {
-    if (!process.env.SAUCE_USERNAME || !process.env.SAUCE_ACCESS_KEY) {
-        console.log('Make sure the SAUCE_USERNAME and SAUCE_ACCESS_KEY environment variables are set.')
-        process.exit(1)
-    }
+    process.env.SAUCE_USERNAME = 'uiappframework';
+    process.env.SAUCE_ACCESS_KEY = 'bbb2b274-6854-456a-aa18-6a311ad10b69';
 
     // Browsers to run on Sauce Labs
-    // Check out https://saucelabs.com/platforms for all browser/OS combos
-    const customLaunchers = {
-        sl_chrome: {
+    var customLaunchers = {
+        'SL_Chrome': {
             base: 'SauceLabs',
-            browserName: 'chrome',
-            version: 'latest'
+            browserName: 'chrome'
         },
-        sl_firefox: {
+        'SL_Firefox': {
             base: 'SauceLabs',
             browserName: 'firefox',
-            version: 'latest'
+            version: '26'
         },
-        sl_ios_safari: {
+        'SL_Safari': {
             base: 'SauceLabs',
-            browserName: 'iphone',
-            platform: ' OS X 10.9',
-            version: 'latest'
+            browserName: 'safari',
+            platform: 'OS X 10.9',
+            version: '7'
         },
-        sl_ie_11: {
+        'SL_IE_9': {
+            base: 'SauceLabs',
+            browserName: 'internet explorer',
+            platform: 'Windows 2008',
+            version: '9'
+        },
+        'SL_IE_10': {
+            base: 'SauceLabs',
+            browserName: 'internet explorer',
+            platform: 'Windows 2012',
+            version: '10'
+        },
+        'SL_IE_11': {
             base: 'SauceLabs',
             browserName: 'internet explorer',
             platform: 'Windows 8.1',
-            version: 'latest'
+            version: '11'
         }
     };
 
-    config.set({
-        basePath: '',
-        frameworks: ['jasmine'],
-        files: [
-            'test/*.js'
+    return {
+        plugins: [
+            require('karma-parallel'),
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('karma-firefox-launcher'),
+            require('karma-safari-launcher'),
+            require('karma-ie-launcher'),
+            require('karma-jasmine-html-reporter'),
+            require('karma-coverage-istanbul-reporter'),
+            require('@angular-devkit/build-angular/plugins/karma'),
+            require('karma-sauce-launcher')
+
         ],
-        reporters: ['progress', 'kjhtml'],
-        port: 9876,
+
+        // base path that will be used to resolve all patterns (eg. files, exclude)
+        basePath: '',
+
+
+        // frameworks to use
+        // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+        frameworks: ['parallel', 'jasmine', '@angular-devkit/build-angular'],
+
+
+        // list of files / patterns to load in the browser
+        // files: [
+        //     /.(js|ts)$/
+        // ],
+
+        dir: join(__dirname, '../coverage'),
+
+
+        // test results reporter to use
+        // possible values: 'dots', 'progress'
+        // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+        reporters: ['dots'],
+
+
+        // web server port
+        port: 9877,
+
         colors: true,
+
+        // level of logging
+        // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+        logLevel: constants.LOG_INFO,
+
         sauceLabs: {
-            testName: 'Build: ' + (process.env.TRAVIS_BUILD_NUMBER || 'Local') + ' - Angular Media Events',
-            recordScreenshots: true,
-            recordVideo: true,
-            connectOptions: {
-                logfile: 'sauce_connect.log'
-            },
-            public: 'public'
+            testName: 'Karma and Sauce Labs demo',
+            dir: join(__dirname, '../../coverage'),
+            startConnect: true
         },
-        // Increase timeout in case connection in CI is slow
         captureTimeout: 120000,
         customLaunchers: customLaunchers,
+
+        // start these browsers
+        // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
         browsers: Object.keys(customLaunchers),
-        singleRun: true,
-        autowatch: true
-    })
+        singleRun: true
+    }
 };
--- libs/core/karma.conf.js
+++ libs/core/karma.conf.js
@@ -3,15 +3,58 @@
 
 const { join } = require('path');
 const getBaseKarmaConfig = require('../../karma.conf');
+const getSauceKarmaConfig = require('../../karma.conf-ci');
+var customLaunchers = {
+    'SL_Chrome': {
+        base: 'SauceLabs',
+        browserName: 'chrome',
+        version: 'latest'
+    },
+    'SL_Firefox': {
+        base: 'SauceLabs',
+        browserName: 'firefox',
+        version: '26'
+        // },
+        // 'SL_Safari': {
+        //     base: 'SauceLabs',
+        //     browserName: 'safari',
+        //     version: '7'
+        // },
+        // 'SL_IE_9': {
+        //     base: 'SauceLabs',
+        //     browserName: 'internet explorer',
+        //     platform: 'Windows 2008',
+        //     version: '9'
+        // },
+        // 'SL_IE_10': {
+        //     base: 'SauceLabs',
+        //     browserName: 'internet explorer',
+        //     platform: 'Windows 2012',
+        //     version: '10'
+        // },
+        // 'SL_IE_11': {
+        //     base: 'SauceLabs',
+        //     browserName: 'internet explorer',
+        //     platform: 'Windows 8.1',
+        //     version: '11'
+        // }
+    };
 
-module.exports = function (config) {
-    const baseConfig = getBaseKarmaConfig();
-    config.set({
-        ...baseConfig,
-        coverageIstanbulReporter: {
-            ...baseConfig.coverageIstanbulReporter,
-            dir: join(__dirname, '../../coverage/libs/core')
-        },
-        browsers: ['ChromeHeadless']
-    });
-};
+    module.exports = function (config) {
+        const baseConfig = getBaseKarmaConfig();
+        const sauceConfig = getSauceKarmaConfig();
+        config.set({
+            ...baseConfig,
+            ...sauceConfig,
+            coverageIstanbulReporter: {
+                ...baseConfig.coverageIstanbulReporter,
+                dir: join(__dirname, '../../coverage/libs/core')
+            },
+            sauceLabs: {
+                ...sauceConfig,
+                dir: join(__dirname, '../../coverage/libs/core')
+            },
+            browsers: Object.keys(customLaunchers)
+            // browsers: ['ChromeHeadless', 'sl_chrome', 'sl_firefox', 'sl_ios_safari', 'sl_ie_11']
+        });
+    };
